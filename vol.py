9079f77070e503d70e4337982e8279fc010d992c
adityaaryan24 <55292577+adityaaryan24@users.noreply.github.com>
2020-09-28 13:27:54 +0530

    Update vol.py

--- vol.py
+++ vol.py
@@ -3,3 +3,15 @@ import pandas as pd
 
 def GARCH(mean, alpha, lam, current, k):
     return (mean**2 + (current**2 - mean**2) * pow(1 - 1/(1 - ((1 - alpha) * (1 - lam))), k))
+
+def c2c():
+    #Function goes here. Refer chapter 10 of Paul Wilmott
+
+def parkinson():
+    #Function goes here
+
+def garman():
+    #Function goes here
+
+def rogers():
+    #Function goes here
