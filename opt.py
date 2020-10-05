7184e5ba9c08d3dd63f11238b1e86281faf600a9
Vedant Khandelwal <63161718+vedant-2001@users.noreply.github.com>
2020-10-05 19:42:59 +0530

    Update opt.py
    added markowitz volatility minimisation function

--- opt.py
+++ opt.py
@@ -33,6 +33,21 @@ class opt:
 
         if port == "volatility":
             #code and return the weights for the portfolio with minimum volatility
+            def portfolio_stats(weights):
+                # Convert to array in case list was passed instead.
+                weights = np.array(weights)
+                port_return = np.sum(df.mean() * weights) * 252
+                port_vol = np.sqrt(np.dot(weights.T, np.dot(df.cov() * 252, weights)))
+                sharpe = port_return/port_vol
+                return {'return': port_return, 'volatility': port_vol, 'sharpe': sharpe}
+            def minimize_volatility(weights):  
+                return portfolio_stats(weights)['volatility'] 
+            constraints = ({'type' : 'eq', 'fun': lambda x: np.sum(x) -1})
+            bounds = tuple((0,1) for x in range(len(df.columns))
+            initializer = num_assets * [1./len(df.columns),]
+            optimal_volatilty=scipy.optimize.minimize(minimize_volatility,initializer,method = 'SLSQP',bounds = bounds,constraints = constraints)
+            x = pd.DataFrame(optimal_volatilty.x,index = df.columns)
+            x = x.to_dict()
 
     def original():
         #Leave this for now
