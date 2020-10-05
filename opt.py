9d85634e58777d8421996371a37f55aa52ca05f4
ishaan886 <68772095+ishaan886@users.noreply.github.com>
2020-10-06 00:29:27 +0530

    Update opt.py

--- opt.py
+++ opt.py
@@ -30,6 +30,26 @@ class opt:
         """
         if port=="sharpe":
             #code and return the weights for maximum sharpe
+            def get_sr(weights):
+                weights = np.array(weights)
+                ret = np.sum(log_return.mean() * weights) * 252
+                vol = np.sqrt(np.dot(weights.T, np.dot(log_return.cov() * 252, weights)))
+                sr = ret/vol
+                return sr
+            def neg_sharpe(weights):
+                 return  get_sr(weights) * -1
+            def check_sum(weights):
+                return np.sum(weights) - 1
+            cons = ({'type':'eq','fun': check_sum})
+            bounds = tuple((0,1) for x in range(len(df.columns)))
+            init_guess=[]
+            for x in range(len(df.columns)):
+                init_guess.append(1/len(df.columns))
+            log_return=np.log(df/df.shift(1))
+            from scipy.optimize import minimize
+            opt_sharp = minimize(neg_sharpe,init_guess,method='SLSQP',bounds=bounds,constraints=cons)
+            result=dict(zip(df.columns,opt_sharp .x))
+            return result
 
         if port == "volatility":
             #code and return the weights for the portfolio with minimum volatility
