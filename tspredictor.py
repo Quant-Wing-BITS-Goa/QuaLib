c3f38a2bc14ae6dc3bf1e8d4f17f87c1201c1ef8
hades-rp2010 <rpatra.rp54@gmail.com>
2021-01-02 14:45:48 +0530

    SARIMAX, SES, HWES

--- tspredictor.py
+++ tspredictor.py
@@ -2,9 +2,13 @@ import numpy as np
 import pandas as pd
 from math import sqrt, pi
 from scipy.stats import norm
+from statsmodels.tsa.api import SimpleExpSmoothing
 from statsmodels.tsa.ar_model import AutoReg
 from statsmodels.tsa.arima_model import ARIMA
 from statsmodels.tsa.arima_model import ARMA
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.vector_ar.var_model import VAR
 from sklearn.metrics import mean_squared_error
 
 
@@ -43,16 +47,16 @@ class tspredictor:
         results = pd.DataFrame(np.column_stack([pred_list, expd_list]), columns = ['Predictions', 'Test'])        
         return results
     
-     def ARMA(self, order):
-          X = self.df.values
+    def ARMA(self, order):
+        X = self.df.values
         train, test = X[1:int(0.90 * len(X))], X[int(0.90 * len(X)):]
         history = [x for x in train]
+        model = ARMA(history, order = order)
+        model_fit = model.fit(disp = 0)
         pred_list = []
         expd_list = []
 
         for i in range(len(test)):
-            model = ARMA(history, order = order)
-            model_fit = model.fit(disp = 0)
             y_hat = model_fit.forecast()[0]
             pred_list.append(y_hat)
             expd_list.append(test[i])
@@ -69,12 +73,12 @@ class tspredictor:
         X = self.df.values
         train, test = X[1:int(0.90 * len(X))], X[int(0.90 * len(X)):]
         history = [x for x in train]
+        model = ARIMA(history, order = order)
+        model_fit = model.fit(disp = 0)
         pred_list = []
         expd_list = []
 
         for i in range(len(test)):
-            model = ARIMA(history, order = order)
-            model_fit = model.fit(disp = 0)
             y_hat = model_fit.forecast()[0]
             pred_list.append(y_hat)
             expd_list.append(test[i])
@@ -87,10 +91,65 @@ class tspredictor:
     
 
     def SARIMAX(self, order, seasonal_order):
+        X = self.df.values
+        train, test = X[1:int(0.90 * len(X))], X[int(0.90 * len(X)):]
+        history = [x for x in train]
+        model = SARIMAX(history, order = order, seasonal_order = seasonal_order)
+        model_fit = model.fit(disp = 0)
+        pred_list = []
+        expd_list = []
 
-    def VAR(self):
+        for i in range(len(test)):
+            y_hat = model_fit.forecast()[0]
+            pred_list.append(y_hat)
+            expd_list.append(test[i])
 
-    def SES(self):
+        error = mean_squared_error(test, pred_list)
+        print('Error:', error)
 
-    def HWES(self):
+        results = pd.DataFrame(np.column_stack([pred_list, expd_list]), columns = ['Predictions', 'Test'])        
+        return results
+
+    def VAR(self, lags = 12, steps=5):
+        pass
+
+    def SES(self, alpha = 0.2, init_method = "heuristic"):
+        X = self.df.values
+        train, test = X[1:int(0.90 * len(X))], X[int(0.90 * len(X)):]
+        history = [x for x in train]
+        model = SimpleExpSmoothing(history, initialization_method = init_method)
+        model_fit = model.fit(smoothing_level = alpha)
+        pred_list = []
+        expd_list = []
+
+        for i in range(len(test)):
+            y_hat = model_fit.forecast()[0]
+            pred_list.append(y_hat)
+            expd_list.append(test[i])
+
+        error = mean_squared_error(test, pred_list)
+        print('Error:', error)
+
+        results = pd.DataFrame(np.column_stack([pred_list, expd_list]), columns = ['Predictions', 'Test'])        
+        return results
+
+    def HWES(self, alpha = 0.2, init_method = "heuristic"):
+        X = self.df.values
+        train, test = X[1:int(0.90 * len(X))], X[int(0.90 * len(X)):]
+        history = [x for x in train]
+        model = ExponentialSmoothing(history, initialization_method = init_method)
+        model_fit = model.fit(smoothing_level = alpha)
+        pred_list = []
+        expd_list = []
+
+        for i in range(len(test)):
+            y_hat = model_fit.forecast()[0]
+            pred_list.append(y_hat)
+            expd_list.append(test[i])
+
+        error = mean_squared_error(test, pred_list)
+        print('Error:', error)
+
+        results = pd.DataFrame(np.column_stack([pred_list, expd_list]), columns = ['Predictions', 'Test'])        
+        return results
 
